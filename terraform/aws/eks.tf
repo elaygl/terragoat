f9dc2cdf9b883575f1da694b14af053f3b28fedf
Elay Gliksberg <elaygl@gmail.com>
2020-12-06 13:44:45 +0200

    Update eks.tf

--- terraform/aws/eks.tf
+++ terraform/aws/eks.tf
@@ -33,7 +33,7 @@ resource aws_iam_role_policy_attachment "policy_attachment-AmazonEKSServicePolic
 resource aws_vpc "eks_vpc" {
   cidr_block           = "10.10.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support   = true
+  enable_dns_support   = false
   tags = {
     Name = "${local.resource_prefix.value}-eks-vpc"
   }
